9c4403eb6e153025175623d3ac9f127cb643665a
maikel233 <maikel233@live.nl>
2022-12-21 22:10:37 +0100

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "unethicalite-plugins"
+
+include("xhunter")
+
+for (project in rootProject.children) {
+    project.apply {
+        projectDir = file(name)
+        buildFileName = "$name.gradle.kts"
+
+        require(projectDir.isDirectory) { "Project '${project.path} must have a $projectDir directory" }
+        require(buildFile.isFile) { "Project '${project.path} must have a $buildFile build script" }
+    }
+}
