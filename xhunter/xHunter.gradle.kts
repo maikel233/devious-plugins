4cd222194ebea46e4995a68b8609047cecb2372a
maikel233 <maikel233@live.nl>
2022-12-21 22:29:09 +0100

    Add files via upload

---
+++ xhunter/xHunter.gradle.kts
@@ -0,0 +1,19 @@
+version = "1.0.0"
+
+project.extra["PluginName"] = "xHunter"
+project.extra["PluginDescription"] = "A hunter plugin Use only in the woodlands"
+
+
+tasks {
+    jar {
+        manifest {
+            attributes(mapOf(
+                    "Plugin-Version" to project.version,
+                    "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
+                    "Plugin-Provider" to project.extra["PluginProvider"],
+                    "Plugin-Description" to project.extra["PluginDescription"],
+                    "Plugin-License" to project.extra["PluginLicense"]
+            ))
+        }
+    }
+}
