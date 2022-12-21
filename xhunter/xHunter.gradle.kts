70c750517f073bd76e0b9f203c6a780d15a301ad
maikel233 <maikel233@live.nl>
2022-12-21 22:23:28 +0100

    Update xHunter.gradle.kts

--- xhunter/xHunter.gradle.kts
+++ xhunter/xHunter.gradle.kts
@@ -1,18 +1,17 @@
-version = "1.0.0"
+version = "0.0.1"
 
 project.extra["PluginName"] = "xHunter"
 project.extra["PluginDescription"] = "A hunter plugin Use only in the woodlands"
-
 
 tasks {
     jar {
         manifest {
             attributes(mapOf(
-                    "Plugin-Version" to project.version,
-                    "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
-                    "Plugin-Provider" to project.extra["PluginProvider"],
-                    "Plugin-Description" to project.extra["PluginDescription"],
-                    "Plugin-License" to project.extra["PluginLicense"]
+                "Plugin-Version" to project.version,
+                "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
+                "Plugin-Provider" to project.extra["PluginProvider"],
+                "Plugin-Description" to project.extra["PluginDescription"],
+                "Plugin-License" to project.extra["PluginLicense"]
             ))
         }
     }
