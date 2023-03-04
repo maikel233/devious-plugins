39b1e9d7895ca0e82c25e77cb39101c4b22a0424
maikel233 <maikel233@live.nl>
2023-03-04 23:50:34 +0100

    Update Waiting.java

--- kourendlibrary/tasks/api/Waiting.java
+++ kourendlibrary/tasks/api/Waiting.java
@@ -21,70 +21,10 @@ import net.unethicalite.api.widgets.Widgets;
 
 public class Waiting {
 
-  public static void moveHumanized(Robot robot, int x, int y, int margin) {
-    // Calculate the random offset for the target coordinates
-    int offsetX = ThreadLocalRandom.current().nextInt(-margin, margin + 1);
-    int offsetY = ThreadLocalRandom.current().nextInt(-margin, margin + 1);
-
-    // Add the offset to the target coordinates
-    int targetX = x + offsetX;
-    int targetY = y + offsetY;
-
-    // Calculate the delay before and after the mouse movement
-    int delayBefore = ThreadLocalRandom.current().nextInt(100, 501);
-    int delayAfter = ThreadLocalRandom.current().nextInt(100, 501);
-
-    // Move the mouse to the target coordinates with the delays and offset applied
-    robot.delay(delayBefore);
-    robot.mouseMove(targetX, targetY);
-    robot.delay(delayAfter);
-  }
-
-  public static void hover(Robot robot, int x, int y, int margin) {
-   // moveHumanized(robot, x, y, margin);
-  }
-
-  public static void move(int x, int y, int margin) {
-    try {
-      Robot robot = new Robot();
-      Random rand = new Random();
-      int newX = x + rand.nextInt(margin) - margin/2;
-      int newY = y + rand.nextInt(margin) - margin/2;
-      robot.mouseMove(newX, newY);
-    } catch (AWTException e) {
-      e.printStackTrace();
-    }
-  }
+  
 
   
-  public static void performActions() {
-    if (!Tabs.isOpen(Tab.SKILLS)) {
-      Tabs.open(Tab.SKILLS);
-    }
-
-    Point CraftingLocation = new Point(643, 348);
-    Point MagicLocation = new Point(579, 378);
-    Point RuneCraftingLocation = new Point(579, 409);
-
-    Random random = new Random();
-    int randomIndex = random.nextInt(3);
-    Point selectedPoint;
-    switch (randomIndex) {
-      case 0:
-        selectedPoint = CraftingLocation;
-        break;
-      case 1:
-        selectedPoint = MagicLocation;
-        break;
-      case 2:
-        selectedPoint = RuneCraftingLocation;
-        break;
-      default:
-        selectedPoint = CraftingLocation; // Default to crafting location if something goes wrong
-    }
-
-    move(selectedPoint.getX(), selectedPoint.getY(), 3);
-  }
+  
 
   public static boolean waitAfterWalking(BooleanSupplier supplier, long timeout) {
     return waitAfterWalking(supplier, timeout, 50);
@@ -92,7 +32,7 @@ public class Waiting {
 
   public static boolean waitAfterWalking(BooleanSupplier supplier, long timeout, long checkEvery) {
     Player local = Players.getLocal();
-    performActions(); // TODO Add something like this?
+  
     if (waitCondition(local::isMoving, 1000)) {
       waitCondition(() -> !local.isMoving(), 8153); // TODO RANDOM THIS!
     }
@@ -100,7 +40,7 @@ public class Waiting {
   }
 
   public static boolean waitWithABC(BooleanSupplier supplier, long timeout) {
-    performActions(); // TODO Add something like this?
+    
     return waitCondition(supplier, timeout);
   }
 
